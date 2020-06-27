18329fe925e95b418ad210d3d3aaaed257363499
Silas HW <silas.hayes.williams@gmail.com>
2020-06-27 23:03:02 +0100

    users can now set reminders with 24 hour time

--- cogs/Useful.py
+++ cogs/Useful.py
@@ -1,5 +1,7 @@
 #modules used
 import re
+import pytz
+from datetime import datetime
 import asyncio
 import discord
 from discord.ext import commands
@@ -11,10 +13,23 @@ class Useful(commands.Cog):
 
     @commands.command(brief="Reminds friends to play games", description="mentions role/user after given amount of minutes has passed\nTo send a multiword message wrap the message with quotes")
     async def reminder(self, ctx, users, timeStr, message="reminder"):
+        
+        if re.search(r"\d\d:\d\d", timeStr):
+            givenHourStr, givenMinuteStr = timeStr.split(":")
 
-        time = int(timeStr)
-        await ctx.send(f"Reminder set for {time} minutes with message '{message}'")
-        await asyncio.sleep(time*60)
+            tz = pytz.timezone('GMT') #sets timezone to be used to get current hour and minute
+            currentHour = datetime.now(tz).hour
+            currentMinute = datetime.now(tz).minute
+
+            hour, minute = int(givenHourStr) - currentHour, int(givenMinuteStr) - currentMinute
+            time = hour*60**2 + minute*60
+
+            await ctx.send(f"Reminder set for {timeStr} with message '{message}'")
+        else:
+            time = int(timeStr)*60
+            await ctx.send(f"Reminder set for {time/60} minutes with message '{message}'")
+        
+        await asyncio.sleep(time)
         await ctx.send(f"{users} {message} *(reminder set by {ctx.message.author})*")
 
 def setup(client):
