8f36cb31d378b5dc7c6b2e27a8932daceadf6147
Silas HW <silas.hayes.williams@gmail.com>
2020-07-22 00:19:16 +0100

    Style help messages

--- cogs/help.py
+++ cogs/help.py
@@ -32,7 +32,7 @@ class Help(commands.Cog):
 
                     #add command name and brief to the help message with formatting
                     helpMessage += f"    **{cmd}**:\n"
-                    helpMessage += f"        *{brief}*\n"
+                    helpMessage += f"    *{brief}*\n"
 
                 helpMessage += "\n"
 
@@ -48,7 +48,7 @@ class Help(commands.Cog):
                     description = 'No description available'
 
                 #add the command name, descriptio and usage to the help message with formatting
-                helpMessage += f"**{command}**\n\n{description}\n\n    *{example}*"
+                helpMessage += f"**{command}**\n{description}\n*{example}*"
             
             #if the command given doesn't exist
             except:
