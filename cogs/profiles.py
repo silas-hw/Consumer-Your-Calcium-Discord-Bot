6505cd5d8c58c53e06d5569a09f15fe8bff881cc
Silas HW <silas.hayes.williams@gmail.com>
2020-07-25 21:15:40 +0100

    Add website link to command descriptions

--- cogs/profiles.py
+++ cogs/profiles.py
@@ -29,7 +29,7 @@ class Profiles(commands.Cog):
         self.sqlreconnect2.start()
 
     #updates users twitter handle in MySQL database
-    @commands.command(brief="Adds your twitter feed to the website", description="Adds your twitter handle to the database so your feed appears on your profile on the website\nSimply type your twitter handle *(without @)* after the command", usage=r"//twitter silas_hw")
+    @commands.command(brief="Adds your twitter feed to the website", description="Adds your twitter handle to the database so your feed appears \non your profile on [the website](http://silashw.heliohost.org)\nSimply type your twitter handle *(without @)* after the command", usage=r"//twitter silas_hw")
     async def twitter(self, ctx, handle):
 
         self.dbcursor.execute(f"UPDATE members SET twitter = '{handle}' WHERE memberid = {ctx.message.author.id}" )
@@ -38,7 +38,7 @@ class Profiles(commands.Cog):
         await ctx.send("<:ballot_box_with_check:730138696069939331> twitter handle updated!")
 
     #adds pinned message to be shown on membe
-    @commands.command(brief="Pin a message!", description="Pins a message to be displayed ors profilen your profile on the website\nThe maximum character length is 50", usage=r"//pin UwU")
+    @commands.command(brief="Pin a message!", description="Pins a message to be displayed on your profile on [the website](http://silashw.heliohost.org)\nThe maximum character length is 50", usage=r"//pin UwU")
     async def pin(self, ctx, *, message):
 
         if len(message) > 50:
@@ -50,7 +50,7 @@ class Profiles(commands.Cog):
             await ctx.send(f"<:pushpin:731608868420845608> *'{message}'* pinned!")
 
     #adds pinned image to be displayed on the website
-    @commands.command(brief="Add an image to your profile!", description="Adds an image to be displayed on your profile page on the website\n The image *will* be stretched", usage=r"//image <attachement>")
+    @commands.command(brief="Add an image to your profile!", description="Adds an image to be displayed on your profile page on [the website](http://silashw.heliohost.org)\n The image *will* be stretched", usage=r"//image <attachement>")
     async def image(self, ctx):
 
         imageUrl = ctx.message.attachments[0].url
