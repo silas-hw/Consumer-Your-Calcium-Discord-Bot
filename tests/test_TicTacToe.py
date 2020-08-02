b8f25896c0acda9e21e79b4363fbe41fb66b370d
Silas HW <silas.hayes.williams@gmail.com>
2020-08-02 20:36:05 +0100

    Add tests

---
+++ tests/test_TicTacToe.py
@@ -0,0 +1,31 @@
+# pylint: disable=import-error
+import pytest
+from games import TicTacToe
+
+@pytest.fixture
+def game():
+    return TicTacToe()
+
+def test_checkWin(game):
+
+    assert game.checkWin('X') == False
+    assert game.checkWin('O') == False
+
+    for position in range(1, 4):
+        game.playerMove('X', position)
+    assert game.checkWin('X') == True
+
+    with pytest.raises(ValueError):
+        game.checkWin('P')
+
+def test_playerMove(game):
+
+    assert game.board[0] == '-'
+    game.playerMove('X', 1)
+    assert game.board[0] == 'X'
+
+def test_display(game):
+
+    assert game.display() == '-  -  -\n-  -  -\n-  -  -\n'
+    game.playerMove('X', 1)
+    assert game.display() == 'X  -  -\n-  -  -\n-  -  -\n'
