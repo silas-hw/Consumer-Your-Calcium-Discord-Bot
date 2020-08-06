a63b1dd4204783c9f0d249657d4c30430429093c
Silas HW <silas.hayes.williams@gmail.com>
2020-08-06 21:13:16 +0100

    Fix mute command

--- cogs/admin.py
+++ cogs/admin.py
@@ -9,16 +9,25 @@ class Admin(commands.Cog):
     @commands.command(brief="mutes a member", description="any message sent by a muted member will be deleted", usage=r"//mute @CleanlyWolf#5407")
     @commands.has_role('Admin')
     async def mute(self, ctx, member: discord.Member):
-        if member in self.mutedMembers[ctx.guild.id]:
+        if member in self.mutedMembers[str(ctx.guild.id)]:
             self.mutedMembers[ctx.guild.id].remove(member)
 
             print(f"\n{ctx.message.author} unmuted {member}")
             await ctx.send(f"<:speaker:727914839615471687> {member} is no longer muted")
         else:
-            self.mutedMembers[ctx.guild.id].append(member)
+            self.mutedMembers[str(ctx.guild.id)].append(member)
 
             print(f"\n{ctx.message.author} muted {member}")
             await ctx.send(f"<:mute:727914643879886929> {member} is now muted")
+
+    @commands.command()
+    @commands.has_role('Admin')
+    async def muted(self, ctx):
+        message = "```Muted members:\n"
+        for member in self.mutedMembers[str(ctx.message.guild.id)]:
+            message += f"    {member}\n"
+        message += "```"
+        await ctx.send(message)
     
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
@@ -26,7 +35,12 @@ class Admin(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.author in self.mutedMembers[message.guild.id]:
+        
+        guildid = str(message.guild.id)
+        if guildid not in self.mutedMembers:
+            self.mutedMembers[guildid] = []
+
+        if message.author in self.mutedMembers[guildid]:
             await message.delete() #deletes message if user is muted
 
 def setup(client):
