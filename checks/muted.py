c5bcf46267764714aba84856915c4d37545b3535
Silas HW <silas.hayes.williams@gmail.com>
2020-08-07 23:32:56 +0100

    Add checks

---
+++ checks/muted.py
@@ -0,0 +1,19 @@
+import json
+import discord
+from discord.ext import commands
+
+class Muted():
+    @staticmethod
+    def check():
+        with open('muted_members.json', 'r') as f:
+            muted = json.load(f)
+
+        def predicate(ctx):
+            return ctx.message.author.id not in muted[str(ctx.guild.id)]
+        return commands.check(predicate)
+
+    @staticmethod
+    def not_muted(guildid, memberid):
+        with open('muted_members.json', 'r') as f:
+            muted = json.load(f)
+        return (memberid not in muted[str(guildid)])
