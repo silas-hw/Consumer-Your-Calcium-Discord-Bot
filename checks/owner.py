1c5eb74cc5cbee9404dfd2bad5c146bf047985c4
Silas HW <silas.hayes.williams@gmail.com>
2020-08-10 15:23:41 +0100

    Remove owner check in favour of builtin check

--- checks/owner.py
+++
@@ -1,9 +0,0 @@
-import discord
-from discord.ext import commands
-
-class Owner():
-    @staticmethod
-    def check():
-        def predicate(ctx):
-            return ctx.message.author.id == 385126151342915588
-        return commands.check(predicate)
