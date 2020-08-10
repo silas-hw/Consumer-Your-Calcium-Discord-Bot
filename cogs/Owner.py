4dd685039b0bea438f75403a3c0e106084dde674
Silas HW <silas.hayes.williams@gmail.com>
2020-08-10 15:23:09 +0100

    Use built in owner check

--- cogs/Owner.py
+++ cogs/Owner.py
@@ -2,9 +2,6 @@ import logging
 import discord
 from discord.ext import commands
 
-#checks
-from checks import Owner
-
 class Owner(commands.Cog):
 
     def __init__(self, client):
@@ -12,7 +9,7 @@ class Owner(commands.Cog):
 
     #used to reload cogs
     @commands.command()
-    @Owner.check()
+    @commands.is_owner()
     async def reload(self, ctx, extension):
         self.client.unload_extension(f"cogs.{extension}")
         self.client.load_extension(f"cogs.{extension}")
@@ -21,7 +18,7 @@ class Owner(commands.Cog):
 
     #used to load cogs
     @commands.command()
-    @Owner.check()
+    @commands.is_owner()
     async def load(self, ctx, extension):
         self.client.load_extension(f"cogs.{extension}")
 
@@ -29,7 +26,7 @@ class Owner(commands.Cog):
 
     #returns logs
     @commands.command()
-    @Owner.check()
+    @commands.is_owner()
     async def logs(self, ctx):
         logs=''
         with open('log.log', 'r') as f:
@@ -38,13 +35,13 @@ class Owner(commands.Cog):
         await ctx.send(f'```css\n{logs}```')
 
     @commands.command(aliases=["announce"], brief="Makes an announcement", description="mention everyone with an announcement sent by the bot", usage=r"//announcement test announcement")
-    @Owner.check()
+    @commands.is_owner()
     async def announcement(self, ctx, *, message):
         await ctx.message.delete() #deletes message used to invoke command
         await ctx.send(f"<@&732354594536947803> {message}")
 
     @commands.command(brief="used to test and debug code", description="Is only available to the owner. Is used to test and debug code and check how things work")
-    @Owner.check()
+    @commands.is_owner()
     async def test(self, ctx):
         messageHistory = await ctx.channel.history(limit=2).flatten()
         await messageHistory[1].add_reaction('🙃')
